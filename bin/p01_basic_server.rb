2afaac1dd7aad26691b2521a526a43f654e615ba
Sean Perfecto <seanperfecto@Seans-MacBook-Pro.local>
2017-04-25 13:57:50 -0700

    Built rack warmup

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,15 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  after = req.path
+  res['Content-Type'] = 'text/html'
+  res.write(after)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
