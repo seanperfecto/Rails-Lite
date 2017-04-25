a380cea267b80e72d9800e5910ca00a404335e10
Sean Perfecto <seanperfecto@Seans-MacBook-Pro.local>
2017-04-25 16:21:11 -0700

    Add session cookie

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -25,6 +25,7 @@ class ControllerBase
       @res.location = url
       @res.status = 302
       @already_built_response = true
+      session.store_session(@res)
   end
 
   # Populate the response with content.
@@ -35,6 +36,7 @@ class ControllerBase
       @res.write(content)
       @res['Content-Type'] = content_type
       @already_built_response = true
+      session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
@@ -49,6 +51,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
