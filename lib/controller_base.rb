382006c36da19f902e357a3a3f87561b455e8332
Sean Perfecto <seanperfecto@Seans-MacBook-Pro.local>
2017-04-25 15:00:05 -0700

    Add template rendering

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -39,6 +40,11 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.name.to_s.underscore
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+    info = File.read(path)
+    content = ERB.new(info).result(binding)
+    render_content(content, 'text/html')
   end
 
   # method exposing a `Session` object
