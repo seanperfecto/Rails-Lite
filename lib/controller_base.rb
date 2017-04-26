e01286c725808693ce4d6cad65c39e2047676ded
Sean Perfecto <seanperfecto@Seans-MacBook-Pro.local>
2017-04-25 18:03:34 -0700

    Fix routes

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -58,6 +58,6 @@ class ControllerBase
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
     self.send(name)
-    render name
+    render(name) unless already_built_response?
   end
 end
